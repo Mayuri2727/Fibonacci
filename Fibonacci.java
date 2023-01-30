556671ed5d39a4ac12aa6cade4d652876ea9806e
Mayuri2727 <mayuriasabe107@gmail.com>
2023-01-30 09:27:40 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 1;
+        System.out.println(1);
+        System.out.println(1);
+
+        for (int i = 1; i <= 98; i++) {
+            int sum = num1 + num2;
+            System.out.println(sum);
+            num1 = num2;
+            num2 = sum;
+        }
+    }
+}
+
